3984d301b50761639bf4ad6116e8a882c8318f2a
Joel Horne <jhorne@ycp.edu>
2024-06-21 09:59:18 -0400

    Draw moving boid

--- Main.py
+++ Main.py
@@ -1,8 +1,24 @@
 from graphics import *
 
-def  main():
-    win = GraphWin(width=200, height=200)
-    win.getMouse()
+g_leftMargin = g_rightMargin = 50
+
+class Boid:
+    def __init__(self, x, y, dx, dy):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+
+def main():
+    boidList = [Boid(10,10,0.005,0)]
+
+    win = GraphWin(title="Boids", width=200, height=200, autoflush=False)
+    curBoid = boidList[0]
+    c = Circle(Point(curBoid.x, curBoid.y), 5)
+    c.draw(win)
+    while(True):
+        c.move(curBoid.dx, curBoid.dy)
+        update()
 
 if __name__ == "__main__":
     main()
